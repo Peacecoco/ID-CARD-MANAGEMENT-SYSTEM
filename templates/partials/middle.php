563c269309a6233a78cedbfb66cf477e770bd024
Peace Olapade <peaceolapade2020@gmail.com>
2026-08-14 15:35:04 +0100

    fix: worked on styling error

--- templates/partials/middle.php
+++ templates/partials/middle.php
@@ -1,78 +1,59 @@
-<?php
-
+<?php 
 ?>
 
+<div class="container">
 
+    <table>
+        <tr>
+            <td>
+                <img
+                    src="<?= htmlspecialchars(__DIR__ . '/../../assets/images/cu_building.jpg') ?>"
+                    width="100%"
+                    class="building-img">
+            </td>
+            <td class="details">
+                <table width="100%" cellpadding="0" cellspacing="0">
+                    <tr><td class="item"><?= htmlspecialchars($student['full_name']) ?></td></tr>
+                    <tr><td style="height: 8mm; font-size: 1px; line-height: 3mm;">&nbsp;</td></tr>
+                    <tr><td class="item"><?= htmlspecialchars($student['department']) ?></td></tr>
+                    <tr><td style="height: 8mm; font-size: 1px; line-height: 3mm;">&nbsp;</td></tr>
+                    <tr><td class="item"><?= htmlspecialchars($student['matric_no']) ?></td></tr>
+                </table>
+            </td>
+        </tr>
 
-<table class="building-table" width="100%" cellpadding="0" cellspacing="0" border="0">
-    <tr>
-        <td class="building-cell" width="50%">
-            <img
-                src="<?= htmlspecialchars(__DIR__ . '/../../assets/images/cu_building.jpg') ?>"
-                width="100%"
-                class="building-img"
-            >
-        </td>
+    </table>
+    
 
-        <td class="red-cell" width="100%">
-             <div class="details">
-                <div class="name">
-                    <?= htmlspecialchars($student['full_name']) ?>
-                </div>
-
-                <div class="dept">
-                    <?= htmlspecialchars($student['department']) ?>
-                </div>
-
-                <div class="matric">
-                    <?= htmlspecialchars($student['matric_no']) ?>
-                </div>
-
-            </div>
-        </td>
-    </tr>
-</table>
+</div>
 
 <style>
+    .container {
+        width: 100%;
+        height: 50mm;
+        box-sizing: border-box;
+        flex-shrink: 0;
+    }
 
-    .building-table {
-        table-layout: fixed;
+    table {
+        width: 100%;
         border-collapse: collapse;
         border-spacing: 0;
+        line-height: 0;
     }
 
-    .building-cell {
+    td {
+        vertical-align: middle;
         padding: 0;
-        margin: 0;
-        vertical-align: top;
+    }
+    .details {
+        background-color: red;
+    }
+    .details .item {
+        padding: auto 5px;
     }
 
-    .building-img {
-        height: 50mm;
-        display: block;
+    .details .item:last-child {
+        padding-bottom: 0;
     }
-.red-cell {
-    width: 90%;
-    height: 50mm;
-    background-color: #ed1b2f;
-    text-align: center;
-    vertical-align: middle;
-    padding: 0;
-    overflow: hidden;
-}
-
-/* .details {
-    width: 100%;
-} */
-
-.name,
-.matric,
-.dept {
-    font-family: Arial, sans-serif;
-    font-size: 14pt;
-    font-weight: bold;
-     line-height: 2;
-}
-
-
 </style>
