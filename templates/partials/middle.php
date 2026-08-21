1c05c2b1035a9a247ab9c5160e4c1becaa8277f2
Peace Olapade <peaceolapade2020@gmail.com>
2026-08-21 01:43:48 +0100

    ...

--- templates/partials/middle.php
+++ templates/partials/middle.php
@@ -1,87 +1,148 @@
 <?php
+$buildingImage = __DIR__ . '/../../assets/images/cu_building.jpg';
 ?>
 <div class="container">
 
-    <table>
+    <table class="middle-table" cellpadding="0" cellspacing="0">
+        <colgroup>
+            <col class="building-column" width="50%">
+            <col class="details-column" width="50%">
+        </colgroup>
         <tr>
+
+            <!-- LEFT: BUILDING IMAGE -->
             <td class="building-cell"></td>
+
+            <!-- RIGHT: STUDENT DETAILS -->
             <td class="details">
-                <table width="100%" cellpadding="0" cellspacing="0">
+
+                <table class="details-table" cellpadding="0" cellspacing="0">
                     <tr>
-                        <td class="item"><?= htmlspecialchars($student['full_name']) ?></td>
+                        <td class="item">
+                            <?= strtoupper(htmlspecialchars($student['full_name'])) ?>
+                        </td>
                     </tr>
+
                     <tr>
-                        <td style="height: 8mm; font-size: 1px; line-height: 3mm;">&nbsp;</td>
+                        <td class="space">&nbsp;</td>
                     </tr>
+
                     <tr>
-                        <td class="item"><?= htmlspecialchars($student['department']) ?></td>
+                        <td class="item">
+                            <?= htmlspecialchars($college['code']) ?>
+                        </td>
                     </tr>
+
                     <tr>
-                        <td style="height: 8mm; font-size: 1px; line-height: 3mm;">&nbsp;</td>
+                        <td class="space">&nbsp;</td>
                     </tr>
+
                     <tr>
-                        <td class="item"><?= htmlspecialchars($student['matric_no']) ?></td>
+                        <td class="item">
+                            <?= htmlspecialchars($student['department']) ?>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td class="space">&nbsp;</td>
+                    </tr>
+
+                    <tr>
+                        <td class="item">
+                            <?= htmlspecialchars($student['matric_no']) ?>
+                        </td>
                     </tr>
                 </table>
-            </td>
-        </tr>
 
+            </td>
+
+        </tr>
     </table>
 
-
 </div>
+
 
 <style>
     .container {
         width: 100%;
         height: <?= MIDDLE_HEIGHT_MM ?>mm;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
         box-sizing: border-box;
-        flex-shrink: 0;
-        margin: 0;
-        padding: 0;
+        background-color: <?= htmlspecialchars($college['primary_color'] ?? '#ff0000') ?>;
     }
 
-    .building-cell {
-    width: 50%;              
-    height: <?= MIDDLE_HEIGHT_MM ?>mm;
-    background-image: url('<?= htmlspecialchars(__DIR__ . '/../../assets/images/cu_building.jpg') ?>');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-}
-    table {
+    /* MAIN TABLE */
+    .middle-table {
         width: 100%;
+        height: <?= MIDDLE_HEIGHT_MM ?>mm;
+        margin: 0;
+        padding: 0;
+        border: 0;
         border-collapse: collapse;
-        border-spacing: 0;
-        line-height: 0;
-        margin: 0;
-        padding: 0;
-        height: 100%;
+        table-layout: fixed;
     }
 
-    tr{
-        height: 100%;
-    }
-
-    td {
-        vertical-align: middle;
-        padding: 0;
-        margin: 0;
-    }
-
-    .details {
-        background-color: <?= htmlspecialchars(
-    $college['primary_color'] ?? '#ff0000'
-) ?>;
+    .building-column,
+    .details-column {
         width: 50%;
     }
 
-    .details .item {
-        padding: 0 5px;
+    /* LEFT SIDE */
+    .building-cell {
+        width: 50%;
+        height: <?= MIDDLE_HEIGHT_MM ?>mm;
+        padding: 0;
         margin: 0;
+        vertical-align: top;
+        background-image: url('<?= htmlspecialchars($buildingImage) ?>');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 100% 100%;
     }
 
-    .details .item:last-child {
-        padding-bottom: 0;
+    /* RIGHT SIDE */
+    .details {
+        width: 50%;
+        height: <?= MIDDLE_HEIGHT_MM ?>mm;
+        padding: 0;
+        margin: 0;
+        vertical-align: middle;
+        overflow: hidden;
+    }
+
+    /* DETAILS TABLE */
+    .details-table {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+        border: 0;
+        border-collapse: collapse;
+        table-layout: fixed;
+    }
+
+    /* TEXT */
+    .item {
+        height: 6mm;
+        width: 100%;
+        padding: 0 2mm;
+        text-align: center;
+        vertical-align: middle;
+        font-size: 10pt;
+        line-height: 4mm;
+        font-weight: bold;
+        word-wrap: break-word;
+        word-break: break-all;
+    }
+
+    /* SPACING */
+    .space {
+        height: 2mm;
+        padding: 0;
+        margin: 0;
+        font-size: 1px;
+        line-height: 1px;
     }
 </style>
